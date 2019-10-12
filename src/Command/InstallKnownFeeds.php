ba30614f9c2b543c974cd6e548cd6fea715467bb
Bjørn Snoen <bjorn@trollweb.no>
2019-10-12 16:36:00 +0200

    [FEATURE] Install known feeds command

---
+++ src/Command/InstallKnownFeeds.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * @license Proprietary
+ * @author Bjørn Snoen <bjorn.snoen@visma.com>
+ * @copyright Visma Digital Commerce 2019
+ */
+
+namespace App\Command;
+
+
+use App\Entity\Feed;
+use App\Entity\Lol;
+use App\Model\Parser\DevHumorParser;
+use App\Model\Parser\GenericParser;
+use App\Model\Parser\RedditParser;
+use App\Model\Parser\XkcdParser;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class InstallKnownFeeds extends Command
+{
+    protected static $defaultName = 'feed:install';
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    protected $knownFeeds = [
+        [
+            'url' => 'https://xkcd.com/rss',
+            'parser' => XkcdParser::class
+        ],
+        [
+            'url' => 'https://www.reddit.com/r/ProgrammerHumor/.rss',
+            'parser' => RedditParser::class
+        ],
+        [
+            'url' => 'http://devhumor.com/feed',
+            'parser' => DevHumorParser::class
+        ],
+        [
+            'url' => 'https://thecodinglove.com/feed',
+            'parser' => GenericParser::class
+        ],
+    ];
+
+    public function __construct(
+        EntityManagerInterface $entityManager
+    ) {
+        parent::__construct(self::$defaultName);
+        $this->entityManager = $entityManager;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        foreach ($this->knownFeeds as $knownFeed) {
+            $feed = new Feed();
+            $feed->setParser($knownFeed['parser'])->setUrl($knownFeed['url']);
+            $this->entityManager->persist($feed);
+        }
+        $this->entityManager->flush();
+        $io = new SymfonyStyle($input, $output);
+        $io->success(":)");
+    }
+}
