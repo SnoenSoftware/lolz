7655711ff770b86b1569ab4b128111804a573be9
Bjørn Snoen <bjorn.snoen@gmail.com>
2021-06-11 16:31:17 +0200

    (feat) Webpack dev hot reload

--- frontend/js/webpack.dev.conf.ts
+++ frontend/js/webpack.dev.conf.ts
@@ -1,23 +1,55 @@
 import { Configuration } from "webpack";
 import RemovePlugin from "remove-files-webpack-plugin";
-import { merge } from 'webpack-merge';
+import ReactRefreshPlugin from "@pmmmwh/react-refresh-webpack-plugin";
+import BabelRefreshPlugin from 'react-refresh/babel';
+import { mergeWithRules } from 'webpack-merge';
+import { WebpackPluginServe } from 'webpack-plugin-serve';
 import { coreConfig } from "./webpack.core.conf";
 
-export const config: Configuration = merge(coreConfig,{
-    devtool: "eval-source-map",
-    mode: "development",
-    plugins: [new RemovePlugin({
-        watch: {
-            include: [
-                "./public/build/main.css"
-            ]
+export const config: Configuration = mergeWithRules({
+    module: {
+        rules: {
+            test: 'match',
+            options: 'replace',
         },
-        before: {
-            include: [
-                "./public/build/main.css"
-            ]
-        }
-    })]
+    }
+})(coreConfig,{
+    devtool: "eval-source-map",
+    entry: [
+        'webpack-plugin-serve/client',
+        './frontend/js/app.tsx'
+    ],
+    mode: "development",
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: "babel-loader",
+                options: {
+                    plugins: [
+                        BabelRefreshPlugin
+                    ],
+                },
+            },
+        ]
+    },
+    plugins: [
+        new RemovePlugin({
+            watch: {
+                include: [
+                    "./public/build/main.css"
+                ]
+            },
+            before: {
+                include: [
+                    "./public/build/main.css"
+                ]
+            }
+        }),
+        new ReactRefreshPlugin(),
+        new WebpackPluginServe()
+    ],
+    watch: true
 });
 
 export default config;
